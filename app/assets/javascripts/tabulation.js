a11c5ddf9611ca05fe72941879f56f5ac75a733a
Christophe BONNEFOY <christophe48bonnefoy@gmail.com>
2019-11-21 15:07:09 +0100

    First commit

---
+++ app/assets/javascripts/tabulation.js
@@ -0,0 +1,13 @@
+/*
+$(document).ready(function() {
+// Mets ici ton code
+  $(".category-content").hide();
+
+  $(".category-title").click(function(){
+    $(".category-content").show();
+  });
+
+  // $(".category-title").on("click",function(){$(".category-content").show();})
+
+});
+*/
